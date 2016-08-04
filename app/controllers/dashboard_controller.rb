693de07d25c82e03f5291c14abf870b07205007f
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-08-04 12:33:35 -0500

    seed data, view dbs on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class DashboardController < ApplicationController
   def hello
+    @users = User.all
+    @matters = Matter.all
+    @user_matters = UserMatter.all
+    
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create! email: "attorney@example.com", username: "attorney1", password: "example"
+User.create! email: "attorney2@example.com", username: "attorney2", password: "example"
+User.create! email: "client1@example.com", username: "client1", password: "example"
+User.create! email: "client2@example.com", username: "client2", password: "example"
+User.create! email: "paralegal@example.com", username: "paralegal", password: "example"
+User.create! email: "admin@example.com", username: "admin", password: "example"
+
+
+Matter.create! cause_number: "2015-201345"
+Matter.create! cause_number: "2016-031305"
+Matter.create! cause_number: "2016-201302"
+Matter.create! cause_number: "2016-921343"
+
+Role.create! name: "Attorney"
+Role.create! name: "Client"
+Role.create! name: "Paralegal"
+Role.create! name: "Admin"
